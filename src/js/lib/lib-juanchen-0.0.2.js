e54ac8932c673102949e045692706393d8f8a9ea
chenjuan <528159689@qq.com>
2018-02-09 10:06:46 +0800

    223

--- src/js/lib/lib-juanchen-0.0.2.js
+++ src/js/lib/lib-juanchen-0.0.2.js
@@ -191,8 +191,43 @@ define(['zepto'],function($){
 	}
 
 	// 【格式化json字符串】
-	juanchen.tools.beautifyJsonString = function(str) {
-		
+	juanchen.tools.beautifyJsonString = function(txt,compress/*是否为压缩模式*/) {
+		var indentChar = '    ';
+        if(/^\s*$/.test(txt)){
+            alert('数据为空,无法格式化! ');
+            return;
+        }
+        try{var data=eval('('+txt+')');}
+        catch(e){
+            alert('数据源语法错误,格式化失败! 错误信息: '+e.description);
+            return;
+        };
+        var draw=[],last=false,This=this,line=compress?'':'\n',nodeCount=0,maxDepth=0;
+
+        var notify=function(name,value,isLast,indent/*缩进*/,formObj){
+            nodeCount++;/*节点计数*/
+            for (var i=0,tab='';i<indent;i++ )tab+=indentChar;/* 缩进HTML */
+            tab=compress?'':tab;/*压缩模式忽略缩进*/
+            maxDepth=++indent;/*缩进递增并记录*/
+            if(value&&value.constructor==Array){/*处理数组*/
+                draw.push(tab+(formObj?('"'+name+'":'):'')+'['+line);/*缩进'[' 然后换行*/
+                for (var i=0;i<value.length;i++)
+                    notify(i,value[i],i==value.length-1,indent,false);
+                draw.push(tab+']'+(isLast?line:(','+line)));/*缩进']'换行,若非尾元素则添加逗号*/
+            }else   if(value&&typeof value=='object'){/*处理对象*/
+                draw.push(tab+(formObj?('"'+name+'":'):'')+'{'+line);/*缩进'{' 然后换行*/
+                var len=0,i=0;
+                for(var key in value)len++;
+                for(var key in value)notify(key,value[key],++i==len,indent,true);
+                draw.push(tab+'}'+(isLast?line:(','+line)));/*缩进'}'换行,若非尾元素则添加逗号*/
+            }else{
+                if(typeof value=='string')value='"'+value+'"';
+                draw.push(tab+(formObj?('"'+name+'":'):'')+value+(isLast?'':',')+line);
+            };
+        };
+        var isLast=true,indent=0;
+        notify('',data,isLast,indent,false);
+        return draw.join('');
 	}
 
 
@@ -397,6 +432,16 @@ define(['zepto'],function($){
 	juanchen.tools.isObject=function(objOrArr){
 		return Object.prototype.toString.call(objOrArr) === "[object Object]";
 	}
+
+	// 【判断是不是空对象】
+	juanchen.tools.isEmptyObj = function(obj) {
+		 for (let key in obj) {
+            if ( obj.hasOwnProperty(key) &&  obj[key] !== '' &&  obj[key] !== null &&  obj[key] !== undefined )  {
+                return false;
+            }
+        }
+        return true;
+	}
 	
 	//【判断是不是ie浏览器】
     juanchen.tools.isIE=function(){
@@ -509,7 +554,7 @@ define(['zepto'],function($){
         
 		用法1：监听容器滚动了多少
 		param = {
-			type: "calcDocScroll",
+			type: "calcWraperScroll",
             scrollContainer: $('.wrap'), // 滚动的容器
             offsetTopCritical: 300,//文档滚动临界点
             callback_in: function () {
